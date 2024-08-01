09117ef0569489f5c9020bc1e8e4eb1c332e6151
Sarthak Mishra <msarthak785@gmail.com>
2024-08-01 11:49:35 -0400

    integrated google's ai

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
-import random
 import time
+import google.generativeai as genai
 
 st.title("OMDENA-MindGuardian")
+
+API_KEY = st.secrets["GOOGLE_API_KEY"]
+
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -11,40 +17,27 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Say something"):
+if question := st.chat_input("Say something"):
     with st.chat_message("human"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        st.markdown(question)
+    st.session_state.messages.append({"role": "user", "content": question})
 
-def response_generator():
-    response = random.choice(
-        [
-            "I am not really a chatbot right now, I am in my building phase. \
-                This is just a test app.",
+    def response_generator(question):
+        prompt = '''
+        You are a seasoned mental health professional specializing in Gen Z.
+        Provide empathetic, evidence-based guidance on anxiety, depression,
+        social media impact, academic stress, and identity exploration.
+        Offer coping strategies, resources, and encourage help-seeking behavior.
+        A gen Z kid is going to ask you a question, answer that in no more than
+        three paragraphs.
+        Question: 
+        ''' + question
+        response = model.generate_content(prompt)
 
-            "Think of me as under construction! I'm still learning and \
-                developing new abilities, so I might not be a fully functional \
-                    chatbot just yet. This is a trial version to gather feedback and \
-                        help me grow into something truly helpful.",
+        for word in response.text.split():
+            yield word + " "
+            time.sleep(0.10)
 
-            "While I can't claim to be a complete chatbot at this stage, \
-                I'm in the exciting early stages of development. This app is a \
-                    stepping stone, allowing us to test my potential and see what \
-                        amazing things I can learn to do.",
-
-            "Imagine me as a chef perfecting a new recipe! This app is like a trial run, \
-                giving us a chance to see how I respond to your questions and interactions. \
-                    It's all part of the process of becoming a truly valuable language model.",
-
-            "Right now, I'm like a complex machine being calibrated. \
-                This test app helps us identify areas where I can improve \
-                    and refine my responses. It's all leading to a more powerful and intelligent future.",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.10)
-
-with st.chat_message("ai"):
-    response = st.write_stream(response_generator())
-st.session_state.messages.append({"role": "ai", "content": response})
+    with st.chat_message("ai"):
+        response = st.write_stream(response_generator(question))
+    st.session_state.messages.append({"role": "ai", "content": response})
